539b16158101da0774abe5b9c22e4e45ef37731e
Carlos Santos <carlos@one-shift.com>
2022-01-14 23:34:49 +0000

    chore: update App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
 import CityWidget from "./components/cityWidget";
 
@@ -7,10 +7,15 @@ import CityWidget from "./components/cityWidget";
 export default function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <CityWidget />
+            <div className={"w-96"}>
+                <div className={"grid grid-cols-2"}>
+                    <div className={"font-light uppercase tracking-wide"}>+ Search</div>
+                    <div className={"text-right font-light uppercase tracking-wide text-gray-300"}>October, 28th</div>
+                </div>
+                <div className={"grid grid-cols-3 gap-40"}>
+                    <CityWidget className="text-gray-800" />
+                </div>
+            </div>
         </>
     )
 }
