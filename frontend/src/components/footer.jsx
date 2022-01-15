5170a9d921fb00e98a3710aa235116140ac5c8b5
Carlos Santos <carlos@one-shift.com>
2022-01-15 22:17:11 +0000

    chore: add logic to footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,15 +1,39 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { RiBarChartFill } from "react-icons/ri";
+import { RiBarChartFill, RiDashboardFill } from "react-icons/ri";
 
-export default function Footer () {
+export default function Footer ({ dashboard, setDashboard, table, setTable, chart, setChart }) {
+
+    function enableDashboard() {
+        setDashboard(true)
+        setTable(false)
+        setChart(false)
+    }
+
+    function enableTable() {
+        setDashboard(false)
+        setTable(true)
+        setChart(false)
+    }
+
+    function enableChart() {
+        setDashboard(false)
+        setTable(false)
+        setChart(true)
+    }
+
     return (
         <>
-            <span>
-                <GiHamburgerMenu className={'inline-block'} /> Table View
-            </span>
-            <span className={"inline-block ml-10"}>
+            {!dashboard ? <span className={"inline-block mx-6 select-none cursor-pointer"} onClick={() => enableDashboard()}>
+                <RiDashboardFill className={"inline-block"} /> Dashboard View
+            </span> : null}
+
+            {!table ? <span className={"inline-block mx-6 select-none cursor-pointer"} onClick={() => enableTable()}>
+                <GiHamburgerMenu className={"inline-block"} /> Table View
+            </span> : null}
+
+            {!chart ? <span className={"inline-block mx-6 select-none cursor-pointer"} onClick={() => enableChart()}>
                 <RiBarChartFill className={'inline-block'} /> Chart View
-            </span>
+            </span> : null}
         </>
     )
 }
