cfb6dbc32d6397953cee792ea959b3a5884787cc
Carlos Santos <carlos@one-shift.com>
2022-01-16 18:34:59 +0000

    chore: update port for backend

--- backend/settings.js
+++ backend/settings.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const port = 3000
+export const port = 8081
 
 export const externalWeatherAPI = "https://api.openweathermap.org/data/2.5"
 export const appId = process.env.WEATHER_API_KEY
