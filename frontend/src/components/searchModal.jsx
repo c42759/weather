8ed5815338110df9f4dd08f966a03e64aa77f37b
Carlos Santos <carlos@one-shift.com>
2022-01-16 17:19:59 +0000

    chore: add ESC handle + error message

--- frontend/src/components/searchModal.jsx
+++ frontend/src/components/searchModal.jsx
@@ -4,19 +4,40 @@ import {getCity} from "../services/cities";
 
 export default function SearchModal({ setCity, disableModal }) {
     const [text, setText] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const errorPool = {"__city_not_found__": "Given city was not found."}
 
     async function handleSubmit(e) {
         e.preventDefault();
 
-        const res = await getCity(text.toLowerCase())
+        try {
+            const res = await getCity(text.toLowerCase())
 
-        let cityName = res.name.toLowerCase()
+            let cityName = res.name.toLowerCase()
 
-        setCity(cityName, res) // Add city to the list
+            setCity(cityName, res) // Add city to the list
 
-        setText("") // Reset field value
-        disableModal()
+            setText("") // Reset field value
+            setErrorMessage("")
+            disableModal()
+        } catch (e) {
+            setErrorMessage(errorPool[e])
+        }
     }
+
+    function handleEsc(event) {
+        if (event.keyCode === 27) {
+            disableModal()
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleEsc)
+        return () => {
+            window.removeEventListener("keydown", handleEsc)
+        };
+    }, []);
 
     return (
         <div className={"absolute top-0 left-0 z-10 w-screen h-full max-h-full bg-white text-center"}>
@@ -28,7 +49,8 @@ export default function SearchModal({ setCity, disableModal }) {
                                    onChange={(e) => setText(e.target.value)}
                                    className={"w-full outline-0 border-b border-b-gray-400 " +
                                        "text-light text-2xl text-center uppercase"} />
-                            <p className={"text-base text-center text-gray-400"}>write a city name and press enter</p>
+                            <p className={"text-base text-center text-gray-400"}>write a city name and press enter, "ESC" to close.</p>
+                            <p className={"text-red-500"}>{errorMessage ? errorMessage : <br />}</p>
                         </form>
                     </div>
                 </div>
