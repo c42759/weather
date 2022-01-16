fd82c2f47436e903065a2673143f1908550a213c
Carlos Santos <carlos@one-shift.com>
2022-01-16 17:22:13 +0000

    chore: add delCity + limit size

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Chart from "./components/chart";
 import TodayDate from "./components/date";
 import Footer from "./components/footer";
 import Search from "./components/search";
+import CityForecast from "./components/cityForecast";
 
 
 export default function App() {
@@ -20,32 +21,39 @@ export default function App() {
         setCities({...cities, [name]: object})
     }
 
-    useEffect(() => {
+    function delCity(cityName) {
+        let tmpCities = {}
 
-    })
+        Object.entries(cities).map(([key, city]) => {
+            if (cityName !== key) {
+                tmpCities[key] = city
+            }
+        })
+
+        setCities(tmpCities)
+    }
 
     return (
-        <>
-            <div className={"text-2xl font-light text-gray-800 px-16 pt-12 pb-4"}>
-                <div className={"text-base grid grid-cols-2 pb-6"}>
-                    <div className={"font-light uppercase tracking-widest"}>
-                        <Search setCity={setCity} />
-                    </div>
-                    <div className={"z-20 text-right font-light uppercase tracking-widest text-gray-400"}>
-                        <TodayDate />
-                    </div>
+        <div className={"container relative mx-auto text-2xl font-light text-gray-800 px-16 pt-12 pb-4 bg-white shadow-lg shadow-gray-500/10"}
+             style={{width: "1280px", height: "720px"}}>
+            <div className={"text-base grid grid-cols-2 pb-6"}>
+                <div className={"font-light uppercase tracking-widest"}>
+                    <Search setCity={setCity} />
                 </div>
-
-                {dashboard ? <Dashboard cities={cities}/> : null}
-                {table ? <Table /> : null}
-                {chart ? <Chart /> : null}
-
-                <div className={"text-base uppercase text-center pt-12"}>
-                    {Object.keys(cities).length ? <Footer dashboard={dashboard} setDashboard={setDashboard}
-                                                          table={table} setTable={setTable}
-                                                          chart={chart} setChart={setChart} /> : null}
+                <div className={"z-20 text-right font-light uppercase tracking-widest text-gray-400"}>
+                    <TodayDate />
                 </div>
             </div>
-        </>
+
+            {dashboard ? <Dashboard cities={cities} delCity={delCity}/> : null}
+            {table ? <Table cities={cities} /> : null}
+            {chart ? <Chart cities={cities} /> : null}
+
+            <div className={"text-base uppercase text-center pt-12"}>
+                {Object.keys(cities).length ? <Footer dashboard={dashboard} setDashboard={setDashboard}
+                                                      table={table} setTable={setTable}
+                                                      chart={chart} setChart={setChart} /> : null}
+            </div>
+        </div>
     )
 }
