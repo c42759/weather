a4876475ff1181262efb2770cb4feff9dfd5f839
Carlos Santos <carlos@one-shift.com>
2022-01-16 10:49:25 +0000

    chore: use moment

--- frontend/src/components/date.jsx
+++ frontend/src/components/date.jsx
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 export default function TodayDate() {
-    let clock = moment().format('MMMM, Do');
+    let clock = moment().format('MMMM, Do')
 
     return (<>{clock}</>)
 }
